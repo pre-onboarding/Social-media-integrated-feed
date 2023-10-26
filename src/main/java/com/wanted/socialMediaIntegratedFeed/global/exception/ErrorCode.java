7f59a52d8bc1b8aeb816f63a58022e4a32846720
ks12b0000 <ks12b0000@gmail.com>
2023-10-26 10:49:52 +0900

    refactor: ErrorCode 수정 #8

--- src/main/java/com/wanted/socialMediaIntegratedFeed/global/exception/ErrorCode.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/global/exception/ErrorCode.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    SERVER_INTERNAL_ERROR(HttpStatus.BAD_REQUEST, "서버 내부적인 에러"),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "중복된 이메일이 있습니다."),
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "중복된 유저네임이 있습니다.");
 
