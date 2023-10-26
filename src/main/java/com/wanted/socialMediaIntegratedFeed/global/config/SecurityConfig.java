850b95bc2e0868f8c6edf5db15c36ef96213cd9f
ks12b0000 <ks12b0000@gmail.com>
2023-10-26 10:54:11 +0900

    refactor: PasswordEncoder Bean 등록 #6

--- src/main/java/com/wanted/socialMediaIntegratedFeed/global/config/SecurityConfig.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/global/config/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.wanted.socialMediaIntegratedFeed.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
 //    @Bean
@@ -15,5 +23,11 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().anyRequest();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
     }
 }
