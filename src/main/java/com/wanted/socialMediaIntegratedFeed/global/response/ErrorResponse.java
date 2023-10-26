5cc8ceca678c63c651a44dcdef1654680063e442
ks12b0000 <ks12b0000@gmail.com>
2023-10-26 10:57:24 +0900

    refactor: ErrorResponse 수정 #8

--- src/main/java/com/wanted/socialMediaIntegratedFeed/global/response/ErrorResponse.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/global/response/ErrorResponse.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
-    private final String errorCode;
+    private final String errorMessage;
     private final String reason;
 }
