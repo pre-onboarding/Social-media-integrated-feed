c95a011af3dd9a981444cb079c9ad74876ab2bb2
MyoungJinSeo <sih5673@gmail.com>
2023-10-27 23:08:57 +0900

    feat : 쿼리 파라미터 추가 #14

--- src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/dto/response/PostPaginationResponse.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/dto/response/PostPaginationResponse.java
@@ -1,16 +1,24 @@
 package com.wanted.socialMediaIntegratedFeed.api.content.dto.response;
 
 
-import jakarta.persistence.Access;
+import com.wanted.socialMediaIntegratedFeed.domain.post.Type;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostPaginationResponse{
     private Long postId;
+    private Type type;
     private String title;
     private String content;
+    private Long viewCount;
+    private Long likeCount;
+    private Long shareCount;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 }
--- src/main/java/com/wanted/socialMediaIntegratedFeed/domain/post/PostRepositoryCustomImpl.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/domain/post/PostRepositoryCustomImpl.java
@@ -1,5 +1,7 @@
 package com.wanted.socialMediaIntegratedFeed.domain.post;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -7,6 +9,7 @@ import com.wanted.socialMediaIntegratedFeed.api.content.dto.response.PostPaginat
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
@@ -19,13 +22,34 @@ import static com.wanted.socialMediaIntegratedFeed.domain.post.QPost.post;
 public class PostRepositoryCustomImpl implements PostRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
+    private OrderSpecifier<?> PostSort(Pageable page) {
+        if (!page.getSort().isEmpty()) {
+            for (Sort.Order order : page.getSort()) {
+                Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+                switch (order.getProperty()){
+                    case "created_at":
+                        return new OrderSpecifier(direction, post.createdAt);
+                    case "updated_at":
+                        return new OrderSpecifier(direction, post.modifiedAt);
+                    case "like_count":
+                        return new OrderSpecifier(direction, post.likeCount);
+                    case "share_count":
+                        return new OrderSpecifier(direction, post.shareCount);
+                    case "view_count":
+                        return new OrderSpecifier(direction, post.viewCount);
+                }
+            }
+        }
+        return null;
+    }
     @Override
     public Page<PostPaginationResponse> findAllByHashtag(Pageable pageable){
 
         List<PostPaginationResponse> posts = jpaQueryFactory.select(Projections.fields(PostPaginationResponse.class,
-                        post.id.as("postId"),post.title,post.content))
+                        post.id.as("postId"),post.type,post.title,post.content,
+                        post.viewCount,post.likeCount,post.shareCount,post.createdAt,post,post.createdAt,post.modifiedAt))
                 .from(post)
-                .orderBy(post.id.desc())
+                .orderBy(PostSort(pageable))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
