1a75ad85065907599ba035651d8f748908036a08
MyoungJinSeo <sih5673@gmail.com>
2023-10-27 23:08:12 +0900

    feat : 쿼리 파라미터 추가 #14

--- src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/controller/PostController.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/controller/PostController.java
@@ -2,18 +2,18 @@ package com.wanted.socialMediaIntegratedFeed.api.content.controller;
 
 import com.wanted.socialMediaIntegratedFeed.api.content.dto.response.PostPaginationResponse;
 import com.wanted.socialMediaIntegratedFeed.api.content.service.PostService;
-import com.wanted.socialMediaIntegratedFeed.domain.hashtag.Hashtag;
+import com.wanted.socialMediaIntegratedFeed.global.common.util.PageableUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/content")
@@ -23,7 +23,23 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public ResponseEntity<Page<PostPaginationResponse>> findAll(Pageable pageable) {
+    public ResponseEntity<Page<PostPaginationResponse>> findAll(
+            @RequestParam("hashtag") String hashtag,
+            @RequestParam("type") String type,
+            @RequestParam("orderBy") String orderBy,
+            @RequestParam("orderBy1") String orderBy1,
+            @RequestParam("searchBy") String searchBy,
+            @RequestParam("search") String search,
+            @RequestParam("pageCount") int pageCount,
+            @RequestParam("page") int page) {
+        Sort sort;
+        if(orderBy1.equals("내림차순") ){
+            sort = Sort.by(Sort.Direction.ASC, orderBy);
+        }
+        else{
+            sort = Sort.by(Sort.Direction.DESC, orderBy);
+        }
+        Pageable pageable = PageableUtil.of(pageCount,page, sort);
 
         Page<PostPaginationResponse> responses = postService.findAllByHashtag(pageable);
 
