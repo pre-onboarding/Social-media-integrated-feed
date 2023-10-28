2c8fd8a5f326f77a43e6fd88418444d77cb63176
MyoungJinSeo <sih5673@gmail.com>
2023-10-29 00:54:20 +0900

    feat : 쿼리 파라미터 #14

--- src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/service/PostService.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/service/PostService.java
@@ -2,14 +2,14 @@ package com.wanted.socialMediaIntegratedFeed.api.content.service;
 
 
 import com.wanted.socialMediaIntegratedFeed.api.content.dto.response.PostPaginationResponse;
-import com.wanted.socialMediaIntegratedFeed.domain.hashtag.Hashtag;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+
 
 public interface PostService {
-    Page<PostPaginationResponse> findAllByHashtag(Pageable pageable);
+    Page<PostPaginationResponse> findAllByHashtag(String hashtag,String type,String searchBy,String search,Pageable pageable);
 
 
 }
