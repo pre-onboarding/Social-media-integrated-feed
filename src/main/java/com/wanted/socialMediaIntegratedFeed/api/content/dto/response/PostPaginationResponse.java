95efb8c57b4cab759c1249e353c5d0b428410688
MyoungJinSeo <sih5673@gmail.com>
2023-10-29 00:54:29 +0900

    feat : 쿼리 파라미터 #14

--- src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/dto/response/PostPaginationResponse.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/dto/response/PostPaginationResponse.java
@@ -1,12 +1,14 @@
 package com.wanted.socialMediaIntegratedFeed.api.content.dto.response;
 
 
+import com.wanted.socialMediaIntegratedFeed.domain.hashtag.Hashtag;
 import com.wanted.socialMediaIntegratedFeed.domain.post.Type;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -19,6 +21,7 @@ public class PostPaginationResponse{
     private Long viewCount;
     private Long likeCount;
     private Long shareCount;
+    private List<Hashtag> hashtags;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 }
