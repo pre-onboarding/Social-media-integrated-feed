eb546be6291a5eff9d4ca88ca9805d46c136d53f
MyoungJinSeo <sih5673@gmail.com>
2023-10-29 00:54:12 +0900

    feat : 쿼리 파라미터 #14

--- src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/service/PostServiceImpl.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/api/content/service/PostServiceImpl.java
@@ -1,17 +1,16 @@
 package com.wanted.socialMediaIntegratedFeed.api.content.service;
 
 import com.wanted.socialMediaIntegratedFeed.api.content.dto.response.PostPaginationResponse;
-import com.wanted.socialMediaIntegratedFeed.domain.post.Post;
+
 import com.wanted.socialMediaIntegratedFeed.domain.post.PostRepository;
-import com.wanted.socialMediaIntegratedFeed.domain.hashtag.Hashtag;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +18,8 @@ import java.util.List;
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     @Override
-    public Page<PostPaginationResponse> findAllByHashtag(Pageable pageable){
-        return postRepository.findAllByHashtag(pageable);
+    public Page<PostPaginationResponse> findAllByHashtag(String hashtag,String type,String searchBy,String search,Pageable pageable){
+        return postRepository.findAllByHashtag(hashtag,type,searchBy,search,pageable);
     }
 
 }
