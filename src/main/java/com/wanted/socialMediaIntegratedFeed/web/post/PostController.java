d85f2be733ed05acbf55f83f18cd2d9dcfcd567c
Seongguk Jeong <seongguk.dev@gmail.com>
2023-10-30 10:46:01 +0900

    refactor: 충돌 사항 수정
    PostController.java. PostService.java, ErrorCode.java, MemberRepository.java

--- src/main/java/com/wanted/socialMediaIntegratedFeed/web/post/PostController.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/web/post/PostController.java
@@ -1,34 +1,56 @@
 package com.wanted.socialMediaIntegratedFeed.web.post;
 
+import com.wanted.socialMediaIntegratedFeed.global.common.dto.PageResponseDto;
+import com.wanted.socialMediaIntegratedFeed.global.common.util.PageableUtil;
+import com.wanted.socialMediaIntegratedFeed.web.post.dto.response.PostPaginationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/post")
 @RequiredArgsConstructor
+@Tag(name = "Post Controller",description = "Post API")
 public class PostController {
 
     private final PostService postService;
+    @Operation(summary = "게시물 목록 api")
+    @ApiResponse(responseCode = "200", description = "조회 성공")
+    @GetMapping
+    public ResponseEntity<PageResponseDto<PostPaginationResponse>> findAll(
+            @RequestParam("hashtag") String hashtag,
+            @RequestParam("type") String type,
+            @RequestParam(value = "orderBy",defaultValue = "created_at") String orderBy,
+            @RequestParam("orderBy1") String orderBy1,
+            @RequestParam(value = "searchBy",defaultValue = "title,content") String searchBy,
+            @RequestParam("search") String search,
+            @RequestParam(value = "pageCount",defaultValue = "10") int pageCount,
+            @RequestParam(value = "page",defaultValue = "0") int page) {
+        Sort sort;
+        if(orderBy1.equals("내림차순") ){
+            sort = Sort.by(Sort.Direction.ASC, orderBy);
+        }
+        else{
+            sort = Sort.by(Sort.Direction.DESC, orderBy);
+        }
+        Pageable pageable = PageableUtil.of(page,pageCount, sort);
+        PageResponseDto<PostPaginationResponse> responses = postService.findAllByHashtag(hashtag,type,searchBy,search,pageable);
 
-    /**
-     * 좋아요 API
-     * @param id
-     * @return
-     */
-    @PatchMapping("/api/v1/content/{id}/like")
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @PatchMapping("/{id}/like")
     public ResponseEntity patchLike(@PathVariable final long id) {
         postService.increaseLike(id);
         return ResponseEntity.ok().build();
     }
-
-    /**
-     * 공유 API
-     * @param id
-     * @return
-     */
-    @PatchMapping("/api/v1/content/{id}/share")
+     
+    @PatchMapping("/{id}/share")
     public ResponseEntity patchShare(@PathVariable final long id) {
         postService.increaseShare(id);
         return ResponseEntity.ok().build();
--- src/main/java/com/wanted/socialMediaIntegratedFeed/web/post/PostService.java
+++ src/main/java/com/wanted/socialMediaIntegratedFeed/web/post/PostService.java
@@ -1,10 +1,15 @@
 package com.wanted.socialMediaIntegratedFeed.web.post;
 
+import com.wanted.socialMediaIntegratedFeed.domain.hashtag.HashtagRepository;
 import com.wanted.socialMediaIntegratedFeed.domain.post.Post;
 import com.wanted.socialMediaIntegratedFeed.domain.post.PostRepository;
+import com.wanted.socialMediaIntegratedFeed.global.common.dto.PageResponseDto;
 import com.wanted.socialMediaIntegratedFeed.global.exception.ErrorCode;
 import com.wanted.socialMediaIntegratedFeed.global.exception.ErrorException;
+import com.wanted.socialMediaIntegratedFeed.web.post.dto.response.PostPaginationResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final HashtagRepository hashtagRepository;
 
     @Transactional
     public void increaseLike(final long id) {
@@ -29,5 +35,14 @@ public class PostService {
     private Post findById(final long id) {
         return postRepository.findById(id)
                 .orElseThrow(() -> new ErrorException(ErrorCode.NOT_FOUND_POST));
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponseDto<PostPaginationResponse> findAllByHashtag(String hashtag, String type, String searchBy, String search, Pageable pageable){
+
+        Long hashtagId = hashtagRepository.findName(hashtag);
+        Page<PostPaginationResponse> allByHashtag = postRepository.findAllByHashtag( type, searchBy,  search, pageable,hashtagId);
+
+        return PageResponseDto.of(allByHashtag);
     }
 }
